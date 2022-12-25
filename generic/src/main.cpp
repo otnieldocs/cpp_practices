bff12c48a73f7613f84f04f2ae9cd076f9ed7716
otnieldocs <otnieldocs@gmail.com>
2022-12-25 11:12:36 +0700

    feat: multiple generic template

--- generic/src/main.cpp
+++ generic/src/main.cpp
@@ -6,8 +6,14 @@ T sum(T a, T b) {
     return a + b;
 }
 
+template <class A, class B>
+bool isEqual(A a, B b) {
+    return a == b;
+}
+
 int main() {
-    int r = sum<int>(5, 3);
-    cout << "5 + 3 = " << r << endl;
+    cout << "5 + 3 = " << sum<int>(5, 3) << endl;
+    cout << "5 == 5 ? " << isEqual(5, 5) << endl;
+    cout << "5 == '5' ? " << isEqual<int, char>(5, '5') << endl;
     return 0;
 }
