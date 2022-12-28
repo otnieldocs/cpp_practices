bbe420255394346e69e71b81d845500ca16067e5
otnieldocs <otnieldocs@gmail.com>
2022-12-28 09:26:52 +0700

    feat: header practice

---
+++ header/src/calc/calc.h
@@ -0,0 +1,8 @@
+#ifndef CALC_H
+#define CALC_H
+
+int sum(int a, int b) {
+    return a + b;
+}
+
+#endif
---
+++ header/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "calc/calc.h"
+using namespace std;
+
+int main() {
+    cout << "5 + 2 = " << sum(5, 2) << endl; 
+    return 0;
+}
